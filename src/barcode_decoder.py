fc113e5944bd9a2c8313a668c4c04321cc9f5b55
Susana <susana.pineda.de.luelmo@gmail.com>
2020-03-22 19:45:11 +0100

    first decoding

--- src/barcode_decoder.py
+++ src/barcode_decoder.py
@@ -7,6 +7,18 @@ class BarcodeDecoder:
     def __init__(self):
         self.value = [-1]
         self._BW_THRESHOLD = 50
+        self._NUMBER_DECODE_MASKS = [[0, 0, 1, 1, 0],   #0
+                                     [1, 0, 0, 0, 1],   #1
+                                     [0, 1, 0, 0, 1],   #2
+                                     [1, 1, 0, 0, 0],   #3
+                                     [0, 0, 1, 0, 1],   #4
+                                     [1, 0, 1, 0, 0],   #5
+                                     [0, 1, 1, 0, 0],   #6
+                                     [0, 0, 0, 1, 1],   #7
+                                     [1, 0, 0, 1, 0],   #8
+                                     [0, 1, 0, 1, 0]]   #9
+        self._BLACK_INDICES = [0, 2, 4, 6, 8]
+        self._WHITE_INDICES = [1, 3, 5, 7, 9]
 
     def decode(self, img):
         h = img.shape[0]
@@ -50,15 +62,31 @@ class BarcodeDecoder:
         max_value = np.amax(cont, initial=2)
         med = int(max_value/2)
 
-        if(np.greater_equal(cont[2:5],med)).all:
+        cont_np = np.array(cont)
+
+        if ((cont_np[2:6]<med).all()):
             print("barcode init OK")
 
+            for i in range(6, len(cont_np)-10, 10):
+                n = cont_np[i:i+10] > med
 
+                black = np.take(n, self._BLACK_INDICES)
+                white = np.take(n, self._WHITE_INDICES)
+                for x in range(9):
+                    if (self._NUMBER_DECODE_MASKS[x] == black).all():
+                        self.value.append(x)
+                        break
+                for x in range(9):
+                    if (self._NUMBER_DECODE_MASKS[x] == white).all():
+                        self.value.append(x)
+                        break
+
+            print(self.value)
+            return self.value
         else:
             print("error in decoder")
             return self.value
 
-        print(cont)
         cv2.imshow("rot",img)
         cv2.waitKey(0)
 
